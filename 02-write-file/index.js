0b1e6f4c3ef85a464108b28af9f449eed087ed3e
aleksawsea <alexandrapynzar57@gmail.com>
2025-01-20 03:18:13 +0400

    feat: implement stopping writing to the file according to the 2nd task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,18 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
-const { stdin: input, stdout: output } = process;
+const { stdin: input } = process;
 
 const filePath = path.join(__dirname, 'test.txt');
 const fileStream = fs.createWriteStream(filePath);
-const rl = readline.createInterface({ input, output: fileStream });
+const rl = readline.createInterface({ input });
 console.log('Hi there! Please, write your content for the created file ^^');
 rl.on('line', (line) => {
-    rl.write(line);
-    // fileStream.write(line);
+    if (line === 'exit') {
+        rl.close();
+    }
+    fileStream.write(line + '\n');
     process.on('SIGINT', () => {
-        process
-    })
+        rl.close();
+    });
 });
 rl.on('error', (err) => {
     console.log(err);
