7300fbfe33fff7c1185bd97d3f1e54207a2d9f71
aleksawsea <alexandrapynzar57@gmail.com>
2025-01-20 03:22:57 +0400

    feat: add the farewell phrase according to the 2nd task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -10,12 +10,16 @@ console.log('Hi there! Please, write your content for the created file ^^');
 rl.on('line', (line) => {
     if (line === 'exit') {
         rl.close();
+    } else {
+        fileStream.write(line + '\n');
+        process.on('SIGINT', () => {
+            rl.close();
+        });
     }
-    fileStream.write(line + '\n');
-    process.on('SIGINT', () => {
-        rl.close();
-    });
 });
+rl.on('close', () => {
+    console.log('Thanks for the cross-check, have a good day! :)')
+})
 rl.on('error', (err) => {
     console.log(err);
 });
