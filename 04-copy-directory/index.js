750eb579be3603082c560db05d7395096ba1a4e4
aleksawsea <alexandrapynzar57@gmail.com>
2025-01-21 01:07:32 +0400

    feat: copy files to the files-copy folder according to the 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,3 +2,25 @@ const fs = require('fs/promises');
 const path = require('path');
 
 const copyFolder = fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
+copyFolder.then(() => {
+    const copyFolderContent = fs.readdir(path.join(__dirname, 'files-copy'));
+    copyFolderContent.then((files) => {
+        files.forEach((file) => {
+            fs.rm(path.join(__dirname, 'files-copy', file));
+        });
+    });
+});
+copyFolder.then(() => {
+    const folderContent = fs.readdir(path.join(__dirname, 'files'));
+    folderContent.then((folderContent) => {
+        folderContent.forEach(file => {
+            copyContent(file);
+        });
+    });
+});
+
+
+async function copyContent(content) {
+    const copy = await fs.copyFile(path.join(__dirname, 'files', content), path.join(__dirname, 'files-copy', content));
+    return copy;
+}
