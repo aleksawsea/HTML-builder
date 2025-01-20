6adc3f166920a74453a5aa1b79603e91f7bd39ad
aleksawsea <alexandrapynzar57@gmail.com>
2025-01-21 01:16:23 +0400

    fix: change then to the asynch functions for the 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,26 +1,21 @@
 const fs = require('fs/promises');
 const path = require('path');
 
-const copyFolder = fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
-copyFolder.then(() => {
-    const copyFolderContent = fs.readdir(path.join(__dirname, 'files-copy'));
-    copyFolderContent.then((files) => {
-        files.forEach((file) => {
-            fs.rm(path.join(__dirname, 'files-copy', file));
-        });
-    });
-});
-copyFolder.then(() => {
-    const folderContent = fs.readdir(path.join(__dirname, 'files'));
-    folderContent.then((folderContent) => {
-        folderContent.forEach(file => {
-            copyContent(file);
-        });
-    });
-});
+async function copyFolderContent() {
+    await fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
+    const existingFiles = await fs.readdir(path.join(__dirname, 'files-copy'));
+    for (const file of existingFiles) {
+        await fs.rm(path.join(__dirname, 'files-copy', file));
+    };
+    const filesForCopying = await fs.readdir(path.join(__dirname, 'files'));
+    for (const file of filesForCopying) {
+        await copyContent(file);
+    };
 
 
-async function copyContent(content) {
-    const copy = await fs.copyFile(path.join(__dirname, 'files', content), path.join(__dirname, 'files-copy', content));
-    return copy;
+    async function copyContent(content) {
+        const copy = await fs.copyFile(path.join(__dirname, 'files', content), path.join(__dirname, 'files-copy', content));
+        return copy;
+    }
 }
+copyFolderContent();
