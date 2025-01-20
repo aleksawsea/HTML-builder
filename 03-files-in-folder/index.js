0f21dc236a3a5e6ef99dab68002c0ab4808903fe
aleksawsea <alexandrapynzar57@gmail.com>
2025-01-20 19:04:38 +0400

    feat: implement file check for the 3rd task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,3 +3,10 @@ const path = require('path');
 
 const secretFolder = path.join(__dirname, 'secret-folder');
 const files = fs.readdir(secretFolder, { withFileTypes: true });
+files.then((files) => {
+    files.forEach((file) => {
+        if (file.isFile()) {
+            console.log(file.isFile());
+        }
+    });
+});
