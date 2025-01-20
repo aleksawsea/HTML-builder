d09332e3770056c8808267275f3c9ec58d8576b2
aleksawsea <alexandrapynzar57@gmail.com>
2025-01-20 21:12:38 +0400

    feat: display data in the console according to the 3rd task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,12 +1,21 @@
 const fs = require('fs/promises');
 const path = require('path');
+// const fs = require('fs');
 
 const secretFolder = path.join(__dirname, 'secret-folder');
 const files = fs.readdir(secretFolder, { withFileTypes: true });
 files.then((files) => {
     files.forEach((file) => {
         if (file.isFile()) {
-            console.log(file.isFile());
+            const fileExt = path.extname(file.name);
+            const fileName = path.basename(file.name, fileExt);
+            let fileSize = 0;
+            const filePath = path.join(secretFolder, file.name);
+            const stat = fs.stat(filePath);
+            stat.then((stat) => {
+                fileSize = (stat.size / 1024).toFixed(3);
+                console.table(`${fileName}  -   ${fileExt}  -   ${fileSize} kb`);
+            });
         }
     });
 });
