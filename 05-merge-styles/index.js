31fc879a9b363c8d08765625c68e704ac79675c2
aleksawsea <alexandrapynzar57@gmail.com>
2025-01-21 03:19:55 +0400

    feat: rewrite code using async function for  the 5th task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,15 +2,18 @@ const fs = require('fs');
 const fsPromises = require('fs/promises');
 const path = require('path');
 
-const bundleFile = path.join(__dirname, 'project-dist', 'bundle.css');
-const writeStream = fs.createWriteStream(bundleFile, { flags: 'a' });
-const styles = fsPromises.readdir(path.join(__dirname, 'styles'), { withFileTypes: true });
-styles.then((styles) => {
-    styles.forEach((file) => {
+async function createBundle() {
+    await fsPromises.rm(path.join(__dirname, 'project-dist', 'bundle.css'), { force: true });
+    const bundleFile = path.join(__dirname, 'project-dist', 'bundle.css');
+    const writeStream = fs.createWriteStream(bundleFile, { flags: 'a' });
+    const styles = await fsPromises.readdir(path.join(__dirname, 'styles'), { withFileTypes: true });
+    for (const file of styles) {
         const fileExt = path.extname(file.name);
         if (file.isFile() && fileExt === '.css') {
             const readStream = fs.createReadStream(path.join(__dirname, 'styles', file.name));
             readStream.pipe(writeStream);
         }
-    });
-})
+    };
+}
+
+createBundle();
