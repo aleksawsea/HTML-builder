a36faf18ce2fe21a20d65efc06c36d529e1b52fb
aleksawsea <alexandrapynzar57@gmail.com>
2025-01-21 02:37:57 +0400

    feat: move styles to the bundle.css according to the 5th task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,8 +1,16 @@
 const fs = require('fs');
+const fsPromises = require('fs/promises');
 const path = require('path');
 
-async function createBundle() {
-    const bundleFile = path.join(__dirname, 'project-dist', 'bundle.css');
-    const stream = fs.createWriteStream(bundleFile);
-}
-createBundle();
+const bundleFile = path.join(__dirname, 'project-dist', 'bundle.css');
+const writeStream = fs.createWriteStream(bundleFile, { flags: 'a' });
+const styles = fsPromises.readdir(path.join(__dirname, 'styles'), { withFileTypes: true });
+styles.then((styles) => {
+    styles.forEach((file) => {
+        const fileExt = path.extname(file.name);
+        if (file.isFile() && fileExt === '.css') {
+            const readStream = fs.createReadStream(path.join(__dirname, 'styles', file.name));
+            readStream.pipe(writeStream);
+        }
+    });
+})
