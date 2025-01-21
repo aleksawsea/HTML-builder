31990ccc893f4fefa08667641db3852dcc3d592f
aleksawsea <alexandrapynzar57@gmail.com>
2025-01-21 04:24:51 +0400

    feat: replace the tags in the template.html according to the 6th task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -8,4 +8,18 @@ async function BuildHTML() {
     for (const file of existingFiles) {
         await fs.rm(path.join(folderPath, file));
     };
+    let components = {};
+    const compFiles = await fs.readdir(path.join(__dirname, 'components'), {withFileTypes: true});
+    for (const file of compFiles) {
+        const content = await fs.readFile(path.join(file.path, file.name), { encoding: 'utf8' });
+        const key = path.basename(file.name, '.html')
+        components[key] = content;
+    };
+    const templateFile = await fs.readFile(path.join(__dirname, 'template.html'), { encoding: 'utf8' });
+    const regExp = /\{\{(\w+)\}\}/g;
+    const replacedFile = templateFile.replace(regExp, (_, placeholder) => {
+        return components[placeholder];
+    });
 }
+BuildHTML();
+
